2dca48e0948feb27cc6657ce23f9974ed318a343
yayaya012 <chart5@outlook.jp>
2024-12-06 19:41:48 +0900

    wip

--- sayingManager.ts
+++ sayingManager.ts
@@ -20,16 +20,18 @@ interface SayingList {
 
 export async function downloadJson(): Promise<SayingList> {
     const command = new GetObjectCommand({ Bucket: BUCKET_NAME, Key: FILE_KEY });
+
+    console.log("BOT_TOKEN", Deno.env.get("BOT_TOKEN"));
+    console.log("AWS_ACCESS_KEY_ID", Deno.env.get("AWS_ACCESS_KEY_ID"));
+    console.log("AWS_SECRET_ACCESS_KEY", Deno.env.get("AWS_SECRET_ACCESS_KEY"));
+    console.log("BUCKET_NAME", Deno.env.get("BUCKET_NAME"));
+
     const response = await s3Client.send(command);
     const body = await new Response(response.Body).text();
     console.log("body", body);
     const parsedData: SayingList = JSON.parse(body);
     console.log("parsedData", parsedData);
 
-    console.log("BOT_TOKEN", Deno.env.get("BOT_TOKEN"));
-    console.log("AWS_ACCESS_KEY_ID", Deno.env.get("AWS_ACCESS_KEY_ID"));
-    console.log("AWS_SECRET_ACCESS_KEY", Deno.env.get("AWS_SECRET_ACCESS_KEY"));
-    console.log("BUCKET_NAME", Deno.env.get("BUCKET_NAME"));
     return parsedData;
 }
 
